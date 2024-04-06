32ebc1d7639ab686e00e261af44049bd5393bfec
Tim Macdonald <macdonald.tim@gmail.com>
2024-04-05 22:47:57 -0400

    Reading server url from env

--- src/counter.ts
+++ src/counter.ts
@@ -1,13 +1,15 @@
+const serverUrl = import.meta.env.VITE_SERVER_URL;
+
 export function setupCounter(element: HTMLButtonElement) {
   
   const incrementCounter = () => {
-    fetch('https://prize-pool-server.fly.dev/counter', { method: 'POST' })
+    fetch(`${serverUrl}/counter`, { method: 'POST' })
       .then(response => response.json())
       .then(response => element.innerHTML = `count is ${response.count}`);
   }
 
   const getCounter = () => {
-    fetch('https://prize-pool-server.fly.dev/counter')
+    fetch(`${serverUrl}/counter`)
       .then(response => response.json())
       .then(response => element.innerHTML = `count is ${response.count}`);
   }
