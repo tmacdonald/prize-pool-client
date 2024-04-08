a1fd35da23fc62f24412e442b4932d008b4d4ac2
Tim Macdonald <macdonald.tim@gmail.com>
2024-04-07 23:55:23 -0400

    Ensuring no duplicates

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,92 @@
-import { QrcodeErrorCallback, QrcodeSuccessCallback } from 'html5-qrcode';
-import { Html5QrcodeError } from 'html5-qrcode/esm/core';
-import { useState } from 'react';
-import './App.css';
-import Html5QrcodePlugin from './components/Html5QrCodePlugin';
-import { Notifications } from './components/Notifications';
-import { PrizeControls } from './components/PrizeControls';
-import { Ticket, submitBallot } from './services/api';
+import { QrcodeErrorCallback, QrcodeSuccessCallback } from "html5-qrcode";
+import { Html5QrcodeError } from "html5-qrcode/esm/core";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import "./App.css";
+import Html5QrcodePlugin from "./components/Html5QrCodePlugin";
+import { Notifications } from "./components/Notifications";
+import { PrizeControls } from "./components/PrizeControls";
+import { Ticket, submitBallot } from "./services/api";
 
 const numPrizes = 5;
 
 function App() {
   const [notifications, setNotifications] = useState<string[]>([]);
   const [prizeId, setPrizeId] = useState<number>(1);
+  const [ticket, setTicket] = useState<
+    [Ticket | undefined, Ticket | undefined]
+  >([undefined, undefined]);
 
-  const handleScan: QrcodeSuccessCallback = (decodedText: string) => {
-    setNotifications(existingNotifications => {
-      const [lastNotification] = existingNotifications.slice(-1);
-      if (!lastNotification || lastNotification !== decodedText) {
-        const ticket = JSON.parse(decodedText) as Ticket;
-        submitBallot(prizeId, ticket);
-        return [ ...existingNotifications, decodedText ];
+  const handleScan = (decodedText: string) => {
+    setTicket(([ticketBefore]) => [
+      JSON.parse(decodedText) as Ticket,
+      ticketBefore,
+    ]);
+  };
+
+  /**
+   * You may be wondering about this useEffect
+   * The issue that I ran into was that, once the QR code scanner has been set up, the onScan function
+   * can't easily be changed, but I needed to update the prizeId without regenerating the onScan function
+   */
+  useEffect(() => {
+    const submitTicket = async () => {
+      /**
+       * This was another interesting case where I wanted to make sure the ticket was changing
+       * before submitting another API request. I needed to store the previous ticket and the new ticket
+       * and verify that we weren't changing the logic. This could be done with a deep equals but I didn't
+       * want to bring in a new library
+       */
+      const [newTicket, previousTicket] = ticket;
+      if (!!newTicket) {
+        if (
+          !previousTicket ||
+          !(
+            previousTicket.participantId === newTicket.participantId &&
+            previousTicket.ticketId === newTicket.ticketId
+          )
+        ) {
+          await submitBallot(prizeId, newTicket);
+          setTicket([newTicket, newTicket]);
+          setNotifications((existingNotifications) => [
+            ...existingNotifications,
+            JSON.stringify(newTicket),
+          ]);
+        }
       }
-      return existingNotifications;
-    });
-  }
+    };
 
-  const handleError: QrcodeErrorCallback = (errorMessage: string, error: Html5QrcodeError) => {
+    submitTicket();
+  }, [prizeId, ticket]);
+
+  const handleError: QrcodeErrorCallback = (
+    errorMessage: string,
+    error: Html5QrcodeError
+  ) => {
     if (error.type !== 0) {
       console.error(errorMessage, error);
     }
-    
-  }
+  };
 
   return (
     <>
       <div>
-        <PrizeControls value={prizeId} onChange={setPrizeId} minPrizeId={1} maxPrizeId={numPrizes} />
-        <Html5QrcodePlugin config={{ fps: 10, qrbox: { width: 250, height: 250 } }} onScan={handleScan} onError={handleError} />
+        <PrizeControls
+          value={prizeId}
+          onChange={setPrizeId}
+          minPrizeId={1}
+          maxPrizeId={numPrizes}
+        />
+        {/* <button onClick={() => handleScan("tim")}>Set to 3</button> */}
+
+        <Html5QrcodePlugin
+          config={{ fps: 10, qrbox: { width: 250, height: 250 } }}
+          onScan={handleScan}
+          onError={handleError}
+        />
         <Notifications notifications={notifications} />
       </div>
     </>
-  )
+  );
 }
 
 export default App;
--- src/components/Html5QrCodePlugin.tsx
+++ src/components/Html5QrCodePlugin.tsx
@@ -1,6 +1,10 @@
-import { Html5Qrcode, QrcodeErrorCallback, QrcodeSuccessCallback } from 'html5-qrcode';
-import { Html5QrcodeScannerConfig } from 'html5-qrcode/esm/html5-qrcode-scanner';
-import { useEffect } from 'react';
+import {
+  Html5Qrcode,
+  QrcodeErrorCallback,
+  QrcodeSuccessCallback,
+} from "html5-qrcode";
+import { Html5QrcodeScannerConfig } from "html5-qrcode/esm/html5-qrcode-scanner";
+import { useEffect } from "react";
 
 const qrcodeRegionId = "html5qr-code-full-region";
 
@@ -23,48 +27,48 @@ interface IProps {
 
 // Creates the configuration object for Html5QrcodeScanner.
 const createConfig = (newConfig: IConfig): Html5QrcodeScannerConfig => {
-    let config: Html5QrcodeScannerConfig = {
-      fps: newConfig.fps
-    };
-    if (newConfig.fps) {
-        config.fps = newConfig.fps;
-    }
-    if (newConfig.qrbox) {
-        config.qrbox = newConfig.qrbox;
-    }
-    if (newConfig.aspectRatio) {
-        config.aspectRatio = newConfig.aspectRatio;
-    }
-    if (newConfig.disableFlip !== undefined) {
-        config.disableFlip = newConfig.disableFlip;
-    }
-    return config;
+  let config: Html5QrcodeScannerConfig = {
+    fps: newConfig.fps,
+  };
+  if (newConfig.fps) {
+    config.fps = newConfig.fps;
+  }
+  if (newConfig.qrbox) {
+    config.qrbox = newConfig.qrbox;
+  }
+  if (newConfig.aspectRatio) {
+    config.aspectRatio = newConfig.aspectRatio;
+  }
+  if (newConfig.disableFlip !== undefined) {
+    config.disableFlip = newConfig.disableFlip;
+  }
+  return config;
 };
 
+const Html5QrcodePlugin = ({
+  config: propsConfig,
+  verbose = false,
+  onScan,
+  onError,
+}: IProps) => {
+  useEffect(() => {
+    // when component mounts
+    const config = createConfig(propsConfig);
+    verbose = verbose === true;
+    // Suceess callback is required.
+    if (!onScan) {
+      throw "qrCodeSuccessCallback is required callback.";
+    }
+    const html5Qrcode = new Html5Qrcode(qrcodeRegionId);
+    html5Qrcode.start({ facingMode: "environment" }, config, onScan, onError);
 
+    // cleanup function when component will unmount
+    return () => {
+      html5Qrcode.clear();
+    };
+  }, []);
 
-const Html5QrcodePlugin = ({ config: propsConfig, verbose = false, onScan, onError } : IProps) => {
-
-    useEffect(() => {
-        // when component mounts
-        const config = createConfig(propsConfig);
-        verbose = verbose === true;
-        // Suceess callback is required.
-        if (!(onScan)) {
-            throw "qrCodeSuccessCallback is required callback.";
-        }
-        const html5Qrcode = new Html5Qrcode(qrcodeRegionId);
-        html5Qrcode.start({ facingMode: "environment" }, config, onScan, onError);
-
-        // cleanup function when component will unmount
-        return () => {
-            html5Qrcode.clear();
-        };
-    }, []);
-
-    return (
-        <div id={qrcodeRegionId} />
-    );
+  return <div id={qrcodeRegionId} />;
 };
 
 export default Html5QrcodePlugin;
