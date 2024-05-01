572a5e2f946d86a21cd3c3f4bc7e10191642625f
Tim Macdonald <macdonald.tim@gmail.com>
2024-05-01 19:57:47 -0400

    Grouped matches

---
+++ src/Events/GroupedMatches.tsx
@@ -0,0 +1,105 @@
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import { generateMatches } from "../services/match";
+import { useBallotStorage, useMatchStorage, usePrizeStorage } from "./hooks";
+import {
+  Checkbox,
+  Container,
+  SpeedDial,
+  SpeedDialAction,
+  SpeedDialIcon,
+} from "@mui/material";
+import { Add, Clear } from "@mui/icons-material";
+import { groupBy } from "lodash";
+
+interface MatchesProps {
+  eventId: string;
+}
+
+export const GroupedMatches = ({ eventId }: MatchesProps) => {
+  const { prizes } = usePrizeStorage(eventId!);
+  const { ballots } = useBallotStorage(eventId!);
+  const { matches, createMatches, deleteAllMatches } = useMatchStorage(
+    eventId!
+  );
+
+  const handleCreateMatches = async () => {
+    const { matches: generatedMatches } = generateMatches(prizes, ballots);
+
+    await deleteAllMatches();
+    await createMatches(...generatedMatches);
+  };
+
+  const handleRemoveAllMatches = () => {
+    deleteAllMatches();
+  };
+
+  const groupedMatches = groupBy(matches, "group");
+
+  const matchTables = Object.entries(groupedMatches).map(
+    ([group, groupMatches]) => (
+      <>
+        <Container>
+          <h2>{group}</h2>
+        </Container>
+        <TableContainer>
+          <Table stickyHeader>
+            <TableHead>
+              <TableRow>
+                <TableCell>Prize</TableCell>
+                <TableCell>Participant</TableCell>
+                <TableCell>Based on Preference</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {groupMatches.map((match) => (
+                <TableRow key={`${match.prizeId}:${match.participantId}`}>
+                  <TableCell>{match.prizeId}</TableCell>
+                  <TableCell>{match.name}</TableCell>
+                  <TableCell>
+                    <Checkbox
+                      checked={match.basedOnPreference}
+                      readOnly={true}
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </>
+    )
+  );
+
+  return (
+    <>
+      <Container>
+        <h1>Matches</h1>
+      </Container>
+      {matchTables}
+      <SpeedDial
+        ariaLabel="SpeedDial basic example"
+        sx={{ position: "fixed", bottom: 16, right: 16 }}
+        icon={<SpeedDialIcon />}
+      >
+        <SpeedDialAction
+          key={"remove"}
+          icon={<Clear />}
+          tooltipTitle={"remove"}
+          onClick={handleRemoveAllMatches}
+        />
+        <SpeedDialAction
+          key={"add-examples"}
+          icon={<Add />}
+          tooltipTitle={"add examples"}
+          onClick={handleCreateMatches}
+        />
+      </SpeedDial>
+    </>
+  );
+};
--- src/Events/MatchesPage.tsx
+++ src/Events/MatchesPage.tsx
@@ -1,8 +1,10 @@
 import { useParams } from "react-router";
 import { Matches } from "./Matches";
+import { GroupedMatches } from "./GroupedMatches";
 
 export const MatchesPage = () => {
   const { eventId } = useParams();
 
-  return <Matches eventId={eventId!} />;
+  // return <Matches eventId={eventId!} />;
+  return <GroupedMatches eventId={eventId!} />;
 };
