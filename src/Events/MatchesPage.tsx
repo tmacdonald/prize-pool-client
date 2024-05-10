9397b9d0183e63fb9a0321e84c330837d5f34394
Tim Macdonald <macdonald.tim@gmail.com>
2024-05-10 01:07:31 -0400

    Switch between grouped and ungrouped matches

--- src/Events/MatchesPage.tsx
+++ src/Events/MatchesPage.tsx
@@ -1,8 +1,21 @@
 import { useParams } from "react-router";
 import { Matches } from "./Matches";
+import { Container, Switch } from "@mui/material";
+import { GroupedMatches } from "./GroupedMatches";
+import { useState } from "react";
 
 export const MatchesPage = () => {
   const { eventId } = useParams();
+  const [grouped, setGrouped] = useState(false);
+
+  return (
+    <Container>
+      <Switch value={grouped} onChange={(_e, checked) => setGrouped(checked)} />{" "}
+      Grouped
+      {!grouped && <Matches eventId={eventId!} />}
+      {grouped && <GroupedMatches eventId={eventId!} />}
+    </Container>
+  );
 
   return <Matches eventId={eventId!} />;
 };
